d69c83c08f4097ccee6d37e3522b631599eebe46
Shen Biluo <sbl1996@126.com>
2019-10-24 10:55:33 +0800

    Remove training

--- hanser/model/segmentation/unet.py
+++ hanser/model/segmentation/unet.py
@@ -3,43 +3,43 @@ from tensorflow.python.keras.layers import ReLU, MaxPool2D, Concatenate
 from hanser.model.layers import bn, conv2d, deconv2d
 
 
-def conv_block(x, channels, training=None):
+def conv_block(x, channels):
     x = conv2d(x, channels, kernel_size=3)
-    x = bn(x, training)
+    x = bn(x)
     x = ReLU()(x)
     x = conv2d(x, channels, kernel_size=3)
-    x = bn(x, training)
+    x = bn(x)
     x = ReLU()(x)
     return x
 
 
-def unet(x, num_classes, channels=64, training=None):
-    c0 = conv_block(x, channels * 1, training)
+def unet(x, num_classes, channels=64):
+    c0 = conv_block(x, channels * 1)
 
     x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(c0)
-    c1 = conv_block(x, channels * 2, training)
+    c1 = conv_block(x, channels * 2)
 
     x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(c1)
-    c2 = conv_block(x, channels * 4, training)
+    c2 = conv_block(x, channels * 4)
 
     x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(c2)
-    c3 = conv_block(x, channels * 8, training)
+    c3 = conv_block(x, channels * 8)
 
     x = MaxPool2D(pool_size=(2, 2), strides=(2, 2))(c3)
-    x = conv_block(x, channels * 16, training)
+    x = conv_block(x, channels * 16)
 
     t = deconv2d(x, channels * 8, kernel_size=2, stride=2)
     x = Concatenate()([c3, t])
-    x = conv_block(x, channels * 8, training)
+    x = conv_block(x, channels * 8)
 
     x = Concatenate()([c2, deconv2d(x, channels * 4, kernel_size=2, stride=2)])
-    x = conv_block(x, channels * 4, training)
+    x = conv_block(x, channels * 4)
 
     x = Concatenate()([c1, deconv2d(x, channels * 2, kernel_size=2, stride=2)])
-    x = conv_block(x, channels * 2, training)
+    x = conv_block(x, channels * 2)
 
     x = Concatenate()([c0, deconv2d(x, channels * 1, kernel_size=2, stride=2)])
-    x = conv_block(x, channels * 1, training)
+    x = conv_block(x, channels * 1)
 
     logits = conv2d(x, num_classes, kernel_size=1, use_bias=True)
 
