d09a2ca50d050cf04ad0ff1a1861b5197419056b
Shen Biluo <sbl1996@126.com>
2019-10-29 14:53:06 +0800

    Add cross_entropy

--- hanser/losses.py
+++ hanser/losses.py
@@ -1,15 +1,20 @@
+from toolz import curry
+
 import tensorflow as tf
 
-
-def cross_entropy(labels, logits, ignore_label=255):
-    # labels = tf.cast(labels, tf.int32)
-
-    num_classes = tf.shape(logits)[-1]
-
-    labels = tf.reshape(labels, [-1])
-    logits = tf.reshape(logits, [-1, num_classes])
-
-    mask = tf.cast(labels != ignore_label, logits.dtype)
-    onehot_labels = tf.one_hot(labels, num_classes, off_value=0, on_value=1)
-    loss = tf.losses.softmax_cross_entropy(onehot_labels, logits, mask)
+@curry
+def cross_entropy(labels, logits, ignore_label=None):
+    r"""
+    Args:
+        labels: (N, H, W)
+        logits: (N, H, W, C)
+    """
+    if ignore_label is not None:
+        mask = tf.not_equal(labels, ignore_label)
+        weights = tf.cast(mask, logits.dtype)
+        labels = tf.where(mask, labels, tf.zeros_like(labels))
+        loss = tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True) * weights
+    else:
+        loss = tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+    loss = tf.reduce_mean(loss)
     return loss
