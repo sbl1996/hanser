c5e737c284f22de22ca0637cef38f6a9076aa0dd
Shen Biluo <sbl1996@126.com>
2019-11-03 09:42:16 +0800

    Fix l2

--- hanser/train/__init__.py
+++ hanser/train/__init__.py
@@ -3,12 +3,15 @@ from tensorflow.keras.regularizers import l2
 
 
 def l2_regularizer(model, alpha):
+    reg = l2(alpha)
     for layer in model.layers:
+        if not layer.trainable:
+            continue
         if isinstance(layer, DepthwiseConv2D):
-            layer.add_loss(l2(alpha)(layer.depthwise_kernel))
+            layer.add_loss(lambda: reg(layer.depthwise_kernel))
         elif isinstance(layer, Conv2D) or isinstance(layer, Dense):
-            layer.add_loss(l2(alpha)(layer.kernel))
+            layer.add_loss(lambda: reg(layer.kernel))
 
         if hasattr(layer, 'bias_regularizer') and layer.use_bias:
-            layer.add_loss(l2(alpha)(layer.bias))
+            layer.add_loss(lambda: reg(layer.bias))
     return model
