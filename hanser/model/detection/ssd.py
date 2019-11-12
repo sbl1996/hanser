f8af4217a009b2e9c8d6a53f0c87ea305df69c5f
Shen Biluo <sbl1996@126.com>
2019-11-12 20:33:34 +0800

    Right init for head

--- hanser/model/detection/ssd.py
+++ hanser/model/detection/ssd.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 
 from tensorflow.keras import Model
-from tensorflow.keras.layers import ReLU, Concatenate, AvgPool2D, UpSampling2D, Input, Lambda, Layer
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.initializers import Constant
 
 from hanser.model.layers import conv2d, bn, relu
-from hanser.model.backbone.resnet import stack1, load_weights, ResNet, block1
+from hanser.model.backbone.resnet import stack1
 
 
 def flat(p, c):
@@ -20,24 +21,10 @@ class DetCat(Layer):
         super().__init__(**kwargs)
         self.num_classes = num_classes
 
-    def compute_output_shape(self, input_shapes):
-        print(input_shapes)
-        n = 0
-        b = input_shapes[0][0]
-        c = self.num_classes
-        for s in input_shapes:
-            ly, lx, o = s[1]
-            n += ly * lx * (o // c)
-        return [(b, n, 4), (b, n, c)]
-
-    def call(self, ps):
-
-        p = tf.concat([
-            flat(p, 4 + self.num_classes)
-            for p in ps
-        ], axis=1)
-        loc_p = p[:, :, :4]
-        cls_p = p[:, :, 4:]
+    def call(self, ps, **kwargs):
+        loc_ps, cls_ps = ps
+        loc_p = tf.concat([flat(p, 4) for p in loc_ps], axis=1)
+        cls_p = tf.concat([flat(p, self.num_classes) for p in cls_ps], axis=1)
         return [loc_p, cls_p]
 
     def get_config(self):
@@ -47,23 +34,25 @@ class DetCat(Layer):
 
 
 def ssd(backbone, num_extras=2, num_anchors=6, num_classes=21):
-    # assert backbone.output_stride == 16
-    # assert len(backbone.outputs) == 2
 
     cs = list(backbone.outputs)
     for i in range(num_extras):
-        cs.append(stack1(cs[-1], 256, 1, stride1=2, name='conv%d' % (i + 6)))
+        cs.append(stack1(cs[-1], 256, 1, stride1=2, name='extra_res%d' % (i + 1)))
 
-    num_outputs = num_anchors * (4 + num_classes)
-    bias_initializer = tf.keras.initializers.Constant(-4.595)
-    ps = [
-        conv2d(c, num_outputs, kernel_size=3, use_bias=True,
-               bias_initializer=bias_initializer,
-               name='pred%d' % (i + 3))
+    loc_ps = [
+        conv2d(c, num_anchors * 4, kernel_size=3, use_bias=True,
+               name='loc_head%d' % (i + 3))
         for i, c in enumerate(cs)
     ]
 
-    loc_p, cls_p = DetCat(num_classes, name='detcat')(ps)
+    cls_ps = [
+        conv2d(c, num_anchors * num_classes, kernel_size=3, use_bias=True,
+               bias_initializer=Constant(-4.595),
+               name='cls_head%d' % (i + 3))
+        for i, c in enumerate(cs)
+    ]
+
+    loc_p, cls_p = DetCat(num_classes, name='detcat')([loc_ps, cls_ps])
 
     model = Model(inputs=backbone.inputs, outputs={'loc_p': loc_p, 'cls_p': cls_p})
     return model
