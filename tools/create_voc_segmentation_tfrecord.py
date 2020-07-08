78fee1f997b2d94f39b995892c84683474879186
sbl1996 <sbl1996@126.com>
2020-07-08 11:18:16 +0800

    add part

--- tools/create_voc_segmentation_tfrecord.py
+++ tools/create_voc_segmentation_tfrecord.py
@@ -7,14 +7,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert VOC Segmentation to TFRecord')
     parser.add_argument('-r', '--root', help='VOC root')
     parser.add_argument('-o', '--output_dir', help='output path')
+    parser.add_argument('-p', '--part', help='part')
     parser.add_argument('-n', '--num_shards', help='number of shards')
     args = parser.parse_args()
 
     root = fmt_path(args.root)
     output_dir = fmt_path(args.output_dir)
+    part = args.part
     num_shards = int(args.num_shards)
 
-    split_f = root / "ImageSets/Segmentation/trainaug.txt"
+    split_f = root / f"ImageSets/Segmentation/{part}.txt"
     image_dir = root / 'JPEGImages'
     label_dir = root / root / 'SegmentationClassAug'
 
