ac6eee8aef622921295f915a9ad7a3195a793cae
sbl1996 <sbl1996@126.com>
2020-10-18 22:40:54 +0800

    Add functional NASNet

---
+++ hanser/models/functional/nasnet.py
@@ -0,0 +1,78 @@
+from tensorflow.keras import Model
+from tensorflow.keras.layers import Activation, Conv2D, BatchNormalization, Concatenate, DepthwiseConv2D, Input, \
+    GlobalAvgPool2D, Dense
+
+
+def ReLUConvBN(x, C_out, kernel_size, stride):
+    x = Activation('relu')(x)
+    x = Conv2D(C_out, kernel_size, strides=stride, padding='same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    return x
+
+
+def FactorizedReduce(x, C_out):
+    assert C_out % 2 == 0
+    x = Activation('relu')(x)
+    x1 = Conv2D(C_out // 2, 1, strides=2, use_bias=False)(x)
+    x2 = Conv2D(C_out // 2, 1, strides=2, use_bias=False)(x[:, 1:, 1:, :])
+    x = Concatenate()([x1, x2])
+    x = BatchNormalization()(x)
+    return x
+
+
+def SepConv(x, C_out, kernel_size, stride):
+    C_in = x.shape[-1]
+    x = Activation('relu')(x)
+    x = DepthwiseConv2D(kernel_size, strides=stride, padding='same', use_bias=False)(x)
+    x = Conv2D(C_in, 1, use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = DepthwiseConv2D(kernel_size, strides=1, padding='same', use_bias=False)(x)
+    x = Conv2D(C_out, 1, use_bias=False)(x)
+    x = BatchNormalization()(x)
+    return x
+
+
+def Cell(s0, s1, C, reduction, reduction_prev):
+    if reduction_prev:
+        s0 = FactorizedReduce(s0, C)
+    else:
+        s0 = ReLUConvBN(s0, C, 1, 1)
+    s1 = ReLUConvBN(s1, C, 1, 1)
+
+    stride = 2 if reduction else 1
+    s2 = SepConv(s0, C, 3, stride) + SepConv(s1, C, 3, stride)
+    s3 = SepConv(s0, C, 3, stride) + SepConv(s1, C, 3, stride)
+    s4 = SepConv(s0, C, 3, stride) + SepConv(s1, C, 3, stride)
+    s5 = SepConv(s0, C, 3, stride) + SepConv(s1, C, 3, stride)
+
+    return Concatenate()([s2, s3, s4, s5])
+
+
+def NASNet(input_size, C, layers, num_classes):
+    inputs = Input(input_size)
+
+    stem_multiplier = 3
+    C_curr = stem_multiplier * C
+    x = Conv2D(C_curr, 3, padding='same', use_bias=False)(inputs)
+    x = BatchNormalization()(x)
+
+    s0 = s1 = x
+
+    C_prev_prev, C_prev, C_curr = C_curr, C_curr, C
+    reduction_prev = False
+    for i in range(layers):
+        if i in [layers // 3, 2 * layers // 3]:
+            C_curr *= 2
+            reduction = True
+        else:
+            reduction = False
+        s0, s1 = s1, Cell(s0, s1, C_curr, reduction, reduction_prev)
+        reduction_prev = reduction
+        C_prev_prev, C_prev = C_prev, 4 * C_curr
+
+    x = GlobalAvgPool2D()(s1)
+    logits = Dense(num_classes)(x)
+
+    model = Model(inputs=inputs, outputs=logits)
+    return model
--- test/test_conv_backward.py
+++ test/test_conv_backward.py
@@ -7,44 +7,52 @@ import tensorflow as tf
 
 from hanser.models.layers import Conv2d
 
-channels = 4
-size = 4
-kernel_size = 1
+channels = 128
+size = 8
+kernel_size = 3
 stride = 1
-dilation = 2
+dilation = 1
 
 def test_impl(size, channels, kernel_size, stride, dilation):
     h = w = size
-    x1 = tf.random.normal([2, h, w, channels])
+    x1 = tf.random.normal([32, h, w, 2])
 
-    m = Conv2d(channels, channels, kernel_size, stride=stride, padding='same',
-               groups=channels, dilation=dilation, bias=False)
-    m.build((None, h, w, channels))
+    m = Conv2d(2, channels, kernel_size, stride=stride, padding='same',
+               groups=1, dilation=dilation, bias=False)
+    m.build((None, h, w, 2))
 
     with tf.GradientTape() as tape:
         y1 = m(x1)
         loss = tf.reduce_sum(y1)
     g = tape.gradient(loss, m.trainable_variables)[0]
 
-
     if isinstance(m, tf.keras.Sequential):
-        weight = m.layers[1].depthwise_kernel
+        conv = m.layers[1]
     else:
-        weight = m.depthwise_kernel
+        conv = m
+    if "Depth" in str(type(conv)):
+        weight = conv.depthwise_kernel
+    else:
+        weight = conv.kernel
     padding = (kernel_size + (kernel_size - 1) * (dilation - 1) - 1) // 2
-    mt = nn.Conv2d(channels, channels, kernel_size, stride=stride, padding=padding,
-                   groups=channels, dilation=dilation, bias=False)
+    mt = nn.Conv2d(2, channels, kernel_size, stride=stride, padding=padding,
+                   groups=1, dilation=dilation, bias=False)
     with torch.no_grad():
-        mt.weight.copy_(torch.from_numpy(np.transpose(weight.numpy(), [2, 3, 0, 1])))
+        if "Depth" in str(type(conv)):
+            T = [2, 3, 0, 1]
+        else:
+            T = [3, 2, 0, 1]
+        mt.weight.copy_(torch.from_numpy(np.transpose(weight.numpy(), T)))
 
     xt1 = torch.from_numpy(np.transpose(x1.numpy(), [0, 3, 1, 2]))
 
     yt1 = mt(xt1)
     loss_t = yt1.sum()
     loss_t.backward()
-    gt = mt.weight.grad.permute(2, 3, 0, 1)
+    gt = mt.weight.grad.numpy()
+    g = np.transpose(g.numpy(), T)
 
-    d = g.numpy() - gt.numpy()
+    d = g - gt
     print(d.mean(), d.std())
     np.testing.assert_allclose(g.numpy(), gt.numpy(), atol=1e-6)
 
