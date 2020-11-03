1d0a075e38def3f8869473d684f19fbaf8f28df5
sbl1996 <sbl1996@126.com>
2020-11-03 10:05:42 +0800

    Add exclude from wd

--- hanser/train/optimizers.py
+++ hanser/train/optimizers.py
@@ -1,4 +1,5 @@
-from typing import Union, Callable
+import re
+from typing import Union, Callable, Optional, List
 
 import tensorflow as tf
 from tensorflow_addons.utils.types import FloatTensorLike
@@ -14,7 +15,9 @@ class SGD(tf.keras.optimizers.Optimizer):
         learning_rate: Union[FloatTensorLike, Callable] = 0.001,
         momentum: FloatTensorLike = 0.9,
         dampening: FloatTensorLike = 0.0,
-        weight_decay=0, nesterov=False,
+        weight_decay: FloatTensorLike = 0,
+        nesterov: bool = False,
+        exclude_from_weight_decay: Optional[List[str]] = None,
         name: str = "SGD",
         **kwargs
     ):
@@ -32,10 +35,20 @@ class SGD(tf.keras.optimizers.Optimizer):
         self._set_hyper("dampening", dampening)
         self._set_hyper("weight_decay", weight_decay)
         self.nesterov = nesterov
+        self.exclude_from_weight_decay = exclude_from_weight_decay
 
     def _create_slots(self, var_list):
         for var in var_list:
             self.add_slot(var, "m")
+
+    def _do_use_weight_decay(self, param_name):
+        """Whether to use L2 weight decay for `param_name`."""
+        if self.exclude_from_weight_decay:
+            for r in self.exclude_from_weight_decay:
+                if re.search(r, param_name) is not None:
+                    return False
+        return True
+
 
     def _prepare_local(self, var_device, var_dtype, apply_state):
         super()._prepare_local(var_device, var_dtype, apply_state)
@@ -57,18 +70,22 @@ class SGD(tf.keras.optimizers.Optimizer):
             (var_device, var_dtype)
         ) or self._fallback_apply_state(var_device, var_dtype)
 
-        # v_t = mu * v + (1 - d) * g_t
         m = self.get_slot(var, "m")
-        grad = grad + coefficients["weight_decay"] * var
+
+        var_name = self._get_variable_name(var.name)
+        if self._do_use_weight_decay(var_name):
+            grad = grad + coefficients["weight_decay"] * var
+
         m_scaled_g_values = grad * coefficients["one_minus_dampening"]
         m_t = m * coefficients["momentum"] + m_scaled_g_values
         m_t = m.assign(m_t, use_locking=self._use_locking)
 
         if self.nesterov:
-            d_p = grad + coefficients["momentum"] * m_t
+            update = grad + coefficients["momentum"] * m_t
         else:
-            d_p = m_t
-        var_update = var - coefficients["lr_t"] * d_p
+            update = m_t
+
+        var_update = var - coefficients["lr_t"] * update
         return var.assign(var_update, use_locking=self._use_locking)
 
     def _resource_apply_sparse(self, grad, var, indices, apply_state=None):
@@ -78,13 +95,20 @@ class SGD(tf.keras.optimizers.Optimizer):
         ) or self._fallback_apply_state(var_device, var_dtype)
 
         m = self.get_slot(var, "m")
+
+        var_name = self._get_variable_name(var.name)
+        if self._do_use_weight_decay(var_name):
+            grad = grad + coefficients["weight_decay"] * var
+
         m_scaled_g_values = grad * coefficients["one_minus_dampening"]
         m_t = m.assign(m * coefficients["momentum"], use_locking=self._use_locking)
         with tf.control_dependencies([m_t]):
             m_t = self._resource_scatter_add(m, indices, m_scaled_g_values)
 
+        update = m_t
+
         var_update = var.assign_sub(
-            coefficients["lr_t"] * m_t, use_locking=self._use_locking
+            coefficients["lr_t"] * update, use_locking=self._use_locking
         )
         return tf.group(*[var_update, m_t])
 
@@ -99,3 +123,120 @@ class SGD(tf.keras.optimizers.Optimizer):
             }
         )
         return config
+
+
+# @tf.keras.utils.register_keras_serializable(package="Hanser")
+# class SGDW(tf.keras.optimizers.Optimizer):
+#
+#     @typechecked
+#     def __init__(
+#         self,
+#         learning_rate: Union[FloatTensorLike, Callable] = 0.001,
+#         momentum: FloatTensorLike = 0.9,
+#         dampening: FloatTensorLike = 0.0,
+#         weight_decay: FloatTensorLike =0,
+#         nesterov: bool = False,
+#         exclude_from_weight_decay: Optional[List[str]] = None,
+#         name: str = "SGD",
+#         **kwargs
+#     ):
+#
+#         super().__init__(name, **kwargs)
+#
+#         # Just adding the square of the weights to the loss function is *not*
+#         # the correct way of using L2 regularization/weight decay with Adam,
+#         # since that will interact with the m and v parameters in strange ways.
+#         #
+#         # Instead we want to decay the weights in a manner that doesn't interact
+#         # with the m/v parameters.
+#         self._set_hyper("learning_rate", kwargs.get("lr", learning_rate))
+#         self._set_hyper("momentum", momentum)
+#         self._set_hyper("dampening", dampening)
+#         self._set_hyper("weight_decay", weight_decay)
+#         self.nesterov = nesterov
+#         self.exclude_from_weight_decay = exclude_from_weight_decay
+#
+#     def _create_slots(self, var_list):
+#         for var in var_list:
+#             self.add_slot(var, "m")
+#
+#     def _do_use_weight_decay(self, param_name):
+#         """Whether to use L2 weight decay for `param_name`."""
+#         if self.exclude_from_weight_decay:
+#             for r in self.exclude_from_weight_decay:
+#                 if re.search(r, param_name) is not None:
+#                     return False
+#         return True
+#
+#     def _prepare_local(self, var_device, var_dtype, apply_state):
+#         super()._prepare_local(var_device, var_dtype, apply_state)
+#
+#         momentum = tf.identity(self._get_hyper("momentum", var_dtype))
+#         dampening = tf.identity(self._get_hyper("dampening", var_dtype))
+#         weight_decay = tf.identity(self._get_hyper("weight_decay", var_dtype))
+#         apply_state[(var_device, var_dtype)].update(
+#             dict(
+#                 momentum=momentum,
+#                 one_minus_dampening=1 - dampening,
+#                 weight_decay=weight_decay,
+#             )
+#         )
+#
+#     def _resource_apply_dense(self, grad, var, apply_state=None):
+#         var_device, var_dtype = var.device, var.dtype.base_dtype
+#         coefficients = (apply_state or {}).get(
+#             (var_device, var_dtype)
+#         ) or self._fallback_apply_state(var_device, var_dtype)
+#
+#         # v_t = mu * v + (1 - d) * g_t
+#         m = self.get_slot(var, "m")
+#         m_scaled_g_values = grad * coefficients["one_minus_dampening"]
+#         m_t = m * coefficients["momentum"] + m_scaled_g_values
+#         m_t = m.assign(m_t, use_locking=self._use_locking)
+#
+#         if self.nesterov:
+#             update = grad + coefficients["momentum"] * m_t
+#         else:
+#             update = m_t
+#
+#         var_name = self._get_variable_name(var.name)
+#         if self._do_use_weight_decay(var_name):
+#             update += coefficients["weight_decay"] * var
+#
+#         var_update = var - coefficients["lr_t"] * update
+#         return var.assign(var_update, use_locking=self._use_locking)
+#
+#     def _resource_apply_sparse(self, grad, var, indices, apply_state=None):
+#         var_device, var_dtype = var.device, var.dtype.base_dtype
+#         coefficients = (apply_state or {}).get(
+#             (var_device, var_dtype)
+#         ) or self._fallback_apply_state(var_device, var_dtype)
+#
+#         m = self.get_slot(var, "m")
+#         m_scaled_g_values = grad * coefficients["one_minus_dampening"]
+#         m_t = m.assign(m * coefficients["momentum"], use_locking=self._use_locking)
+#         with tf.control_dependencies([m_t]):
+#             m_t = self._resource_scatter_add(m, indices, m_scaled_g_values)
+#
+#         update = m_t
+#
+#         var_name = self._get_variable_name(var.name)
+#         if self._do_use_weight_decay(var_name):
+#             update += coefficients["weight_decay"] * var
+#
+#         var_update = var.assign_sub(
+#             coefficients["lr_t"] * update, use_locking=self._use_locking
+#         )
+#         return tf.group(*[var_update, m_t])
+#
+#     def get_config(self):
+#         config = super().get_config()
+#         config.update(
+#             {
+#                 "learning_rate": self._serialize_hyperparameter("learning_rate"),
+#                 "momentum": self._serialize_hyperparameter("momentum"),
+#                 "dampening": self._serialize_hyperparameter("dampening"),
+#                 "weight_decay": self._serialize_hyperparameter("weight_decay"),
+#             }
+#         )
+#         return config
