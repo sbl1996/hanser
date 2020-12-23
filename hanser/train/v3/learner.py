0803f40c2836f16bbf5e6e284469f354a3e6aa1f
sbl1996 <sbl1996@126.com>
2020-12-23 20:15:07 +0800

    Fix

--- hanser/train/v3/learner.py
+++ hanser/train/v3/learner.py
@@ -208,7 +208,7 @@ class Learner(metaclass=ABCMeta):
     def _test_step(self, inputs):
         strategy_run(self._strategy, self.test_batch, (inputs,))
 
-    @tf.function
+    @tf.function(experimental_relax_shapes=True)
     def _run_steps(self, step_fn, iterator, n_steps, callbacks, state):
         for i in tf.range(n_steps):
             batch = next(iterator)
