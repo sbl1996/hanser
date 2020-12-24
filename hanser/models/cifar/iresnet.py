474dcaba6e7770153a464265dd109b3850d7cc69
sbl1996 <sbl1996@126.com>
2020-12-24 16:02:26 +0800

    Add res2net

--- hanser/models/cifar/iresnet.py
+++ hanser/models/cifar/iresnet.py
@@ -67,28 +67,63 @@ class Bottleneck(Layer):
 
     expansion = 4
 
-    def __init__(self, in_channels, out_channels, stride, erase_relu):
+    def __init__(self, in_channels, out_channels, stride,
+                 start_block=False, end_block=False, exclude_bn0=False):
         super().__init__()
         channels = out_channels // self.expansion
-        self.conv1 = Conv2d(in_channels, channels, kernel_size=1,
-                            norm='def', act='def')
-        self.conv2 = Conv2d(channels, channels, kernel_size=3, stride=stride,
-                            norm='def', act='def')
-        self.conv3 = Conv2d(channels, out_channels, kernel_size=1,
-                            norm='def')
+        if not start_block and not exclude_bn0:
+            self.bn0 = Norm(in_channels)
+        if not start_block:
+            self.act0 = Act()
+        self.conv1 = Conv2d(in_channels, channels, kernel_size=1)
+        self.bn1 = Norm(channels)
+        self.act1 = Act()
+        self.conv2 = Conv2d(channels, channels, kernel_size=3, stride=stride)
+        self.bn2 = Norm(channels)
+        self.act2 = Act()
+        self.conv3 = Conv2d(channels, out_channels, kernel_size=1)
+
+        if start_block:
+            self.bn3 = Norm(out_channels)
+
+        if end_block:
+            self.bn3 = Norm(out_channels)
+            self.act3 = Norm(out_channels)
+
         if stride != 1 or in_channels != out_channels:
-            self.shortcut = Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, norm='def')
+            shortcut = []
+            if stride != 1:
+                shortcut.append(Pool2d(2, 2, type='avg'))
+            shortcut.append(
+                Conv2d(in_channels, out_channels, kernel_size=1, norm='def'))
+            self.shortcut = Sequential(shortcut)
         else:
             self.shortcut = Identity()
-        self.act = Act() if not erase_relu else Identity()
+        self.start_block = start_block
+        self.end_block = end_block
+        self.exclude_bn0 = exclude_bn0
 
     def call(self, x):
         identity = self.shortcut(x)
-        x = self.conv1(x)
+        if self.start_block:
+            x = self.conv1(x)
+        else:
+            if not self.exclude_bn0:
+                x = self.bn0(x)
+            x = self.act0(x)
+            x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.act1(x)
         x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.act2(x)
         x = self.conv3(x)
+        if self.start_block:
+            x = self.bn3(x)
         x = x + identity
-        x = self.act(x)
+        if self.end_block:
+            x = self.bn3(x)
+            x = self.act3(x)
         return x
 
 
---
+++ hanser/models/cifar/res2net/layers.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+from hanser.models.layers import Conv2d, Identity, Pool2d
+
+
+class StartRes2Conv(Layer):
+
+    def __init__(self, channels, kernel_size, stride=1, scale=4, norm='def', act='def'):
+        super().__init__()
+        assert channels % scale == 0
+        width = channels // scale
+        self.direct = Identity() if stride == 1 else Pool2d(3, stride, type='avg')
+        self.conv = Conv2d(width * (scale - 1), width * (scale - 1), kernel_size=kernel_size, stride=stride,
+                            norm=norm, act=act)
+        self.width = width
+
+    def call(self, x):
+        xs = tf.split(x, [self.width, -1], axis=-1)
+        outs = [self.direct(xs[0]), self.conv(xs[1])]
+        x = tf.concat(outs, axis=-1)
+        return x
+
+
+class MainRes2Conv(Layer):
+
+    def __init__(self, channels, kernel_size, stride=1, scale=4, norm='def', act='def'):
+        super().__init__()
+        assert channels % scale == 0
+        width = channels // scale
+        self.scale = scale
+        self.direct = Identity() if stride == 1 else Pool2d(3, stride, type='avg')
+        self.convs = [
+            Conv2d(width, width, kernel_size=kernel_size, stride=stride, norm=norm, act=act)
+            for i in range(scale - 1)
+        ]
+
+    def call(self, x):
+        xs = tf.split(x, self.scale, axis=-1)
+        outs = [
+            self.direct(xs[0]),
+            self.convs[0](xs[1])
+        ]
+        for i in range(2, self.scale):
+            x = outs[-1] + xs[i]
+            x = self.convs[i - 1](x)
+            outs.append(x)
+        x = tf.concat(outs, axis=-1)
+        return x
+
+
+def Res2Conv(channels, kernel_size, stride, scale, norm, act, start_block):
+    if start_block:
+        return StartRes2Conv(channels, kernel_size, stride, scale, norm, act)
+    else:
+        return MainRes2Conv(channels, kernel_size, stride, scale, norm, act)
