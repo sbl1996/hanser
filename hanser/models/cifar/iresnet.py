916086f1da84f3c69a58bf0f49abbfd857a08068
sbl1996 <sbl1996@126.com>
2020-12-24 14:25:19 +0800

    Add drop_rate for IResNet

--- hanser/models/cifar/iresnet.py
+++ hanser/models/cifar/iresnet.py
@@ -1,11 +1,11 @@
 from tensorflow.keras import Sequential, Model
-from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Layer, Dropout
 
 from hanser.models.layers import Conv2d, Act, Identity, GlobalAvgPool, Linear, Norm, Pool2d
 
 
 class BasicBlock(Layer):
-    def __init__(self, in_channels, out_channels, stride,
+    def __init__(self, in_channels, out_channels, stride, drop_rate,
                  start_block=False, end_block=False, exclude_bn0=False):
         super().__init__()
         if not start_block and not exclude_bn0:
@@ -17,6 +17,7 @@ class BasicBlock(Layer):
         self.conv1 = Conv2d(in_channels, out_channels, kernel_size=3, stride=stride)
         self.bn1 = Norm(out_channels)
         self.act1 = Act()
+        self.dropout = Dropout(drop_rate) if drop_rate else Identity()
         self.conv2 = Conv2d(out_channels, out_channels, kernel_size=3)
 
         if start_block:
@@ -51,6 +52,7 @@ class BasicBlock(Layer):
             x = self.conv1(x)
         x = self.bn1(x)
         x = self.act1(x)
+        x = self.dropout(x)
         x = self.conv2(x)
         if self.start_block:
             x = self.bn2(x)
@@ -92,7 +94,7 @@ class Bottleneck(Layer):
 
 class ResNet(Model):
 
-    def __init__(self, depth, block='basic', num_classes=10, stages=(16, 16, 32, 64)):
+    def __init__(self, depth, block='basic', drop_rate=0.3, num_classes=10, stages=(16, 16, 32, 64)):
         super().__init__()
         self.stages = stages
         if block == 'basic':
@@ -105,20 +107,25 @@ class ResNet(Model):
         self.conv = Conv2d(3, self.stages[0], kernel_size=3, norm='def', act='def')
 
         self.layer1 = self._make_layer(
-            block, self.stages[0], self.stages[1], layers[0], stride=1)
+            block, self.stages[0], self.stages[1], layers[0], stride=1,
+            drop_rate=drop_rate)
         self.layer2 = self._make_layer(
-            block, self.stages[1], self.stages[2], layers[1], stride=2)
+            block, self.stages[1], self.stages[2], layers[1], stride=2,
+            drop_rate=drop_rate)
         self.layer3 = self._make_layer(
-            block, self.stages[2], self.stages[3], layers[2], stride=2)
+            block, self.stages[2], self.stages[3], layers[2], stride=2,
+            drop_rate=drop_rate)
 
         self.avgpool = GlobalAvgPool()
         self.fc = Linear(self.stages[3], num_classes)
 
-    def _make_layer(self, block, in_channels, out_channels, blocks, stride=1):
-        layers = [block(in_channels, out_channels, stride=stride, start_block=True)]
+    def _make_layer(self, block, in_channels, out_channels, blocks, stride, drop_rate):
+        layers = [block(in_channels, out_channels, stride=stride, start_block=True,
+                        drop_rate=drop_rate)]
         for i in range(1, blocks):
             layers.append(block(out_channels, out_channels, stride=1,
-                                exclude_bn0=i == 1, end_block=i == blocks - 1))
+                                exclude_bn0=i == 1, end_block=i == blocks - 1,
+                                drop_rate=drop_rate))
         return Sequential(layers)
 
     def call(self, x):
