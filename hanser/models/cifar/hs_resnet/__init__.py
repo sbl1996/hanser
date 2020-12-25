dce3e34af6964b577c47e3f12fcfab7a50333e4d
sbl1996 <sbl1996@126.com>
2020-12-25 11:11:33 +0800

    Add HSResNet

--- hanser/models/cifar/hs_resnet/__init__.py
+++ hanser/models/cifar/hs_resnet/__init__.py
@@ -1,2 +1,1 @@
-from hanser.models.cifar.res2net.resnet_vd import ResNet as ResNetvd
-from hanser.models.cifar.res2net.iresnet import ResNet as IResNet
+from hanser.models.cifar.hs_resnet.iresnet import ResNet as IResNet
