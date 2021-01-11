22ae76185cd0b5c5d052463799cf92c0bf9b9906
sbl1996 <sbl1996@126.com>
2021-01-11 22:28:46 +0800

    Add

--- hanser/models/imagenet/iresnet/resnet_vd.py
+++ hanser/models/imagenet/iresnet/resnet_vd.py
@@ -1,7 +1,6 @@
 from tensorflow.keras import Sequential, Model
-from tensorflow.keras.layers import Layer, Dropout
+from tensorflow.keras.layers import Layer
 
-from hanser.models.modules import DropPath
 from hanser.models.layers import Conv2d, Act, Identity, GlobalAvgPool, Linear, Norm, Pool2d
 
 
