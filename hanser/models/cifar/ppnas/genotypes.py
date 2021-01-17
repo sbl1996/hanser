6b5dc13e6bab9084c04b49735e22d9912c291167
sbl1996 <sbl1996@126.com>
2021-01-17 15:33:38 +0800

    Add

--- hanser/models/cifar/ppnas/genotypes.py
+++ hanser/models/cifar/ppnas/genotypes.py
@@ -10,3 +10,21 @@ PP_ResNet_1 = Genotype(
         (5, 7, 'nor_conv_3x3'),
     ],
 )
+
+PP_ResNet_2 = Genotype(
+    normal=[
+        (3, 4, 'nor_conv_3x3'),
+        (1, 5, 'nor_conv_3x3'),
+        (2, 6, 'nor_conv_3x3'),
+        (1, 7, 'nor_conv_3x3'),
+    ],
+)
+
+PP_ResNet_3 = Genotype(
+    normal=[
+        (1, 3, 'nor_conv_3x3'),
+        (3, 4, 'nor_conv_3x3'),
+        (2, 6, 'nor_conv_3x3'),
+        (2, 7, 'nor_conv_3x3'),
+    ],
+)
