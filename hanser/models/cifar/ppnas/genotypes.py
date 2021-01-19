b9847e597ceb292f90be8a584758a9ff1a616381
sbl1996 <sbl1996@126.com>
2021-01-19 10:27:44 +0800

    Add

--- hanser/models/cifar/ppnas/genotypes.py
+++ hanser/models/cifar/ppnas/genotypes.py
@@ -55,3 +55,46 @@ PP_ResNet_ImageNet_NS_2 = Genotype(
         [(3, 1, 'nor_conv_3x3'), (2, 5, 'nor_conv_3x3'), (6, 2, 'nor_conv_3x3'), (4, 7, 'nor_conv_3x3')]
     ],
 )
+
+PP_ResNet_CIFAR10_FAIR_1 = Genotype([
+    [
+        (1, 2, 3, 4, 'nor_conv_3x3'),
+        (3, 5, 'nor_conv_3x3'),
+        (1, 2, 4, 5, 'nor_conv_3x3'),
+        (1, 3, 4, 5, 6, 7, 'nor_conv_3x3')
+    ],
+    [
+        (1, 4, 'nor_conv_3x3'),
+        (1, 2, 3, 5, 'nor_conv_3x3'),
+        (2, 5, 'nor_conv_3x3'),
+        (1, 4, 6, 'nor_conv_3x3')
+    ],
+    [
+        (1, 'nor_conv_3x3'),
+        (1, 2, 4, 5, 'nor_conv_3x3'),
+        (4, 5, 6, 'nor_conv_3x3'),
+        (1, 2, 4, 5, 7, 'nor_conv_3x3')
+    ]
+])
+
+
+PP_ResNet_CIFAR100_FAIR_1 = Genotype([
+    [
+        (2, 4, 'nor_conv_3x3'),
+        (1, 2, 4, 5, 'nor_conv_3x3'),
+        (1, 2, 5, 6, 'nor_conv_3x3'),
+        (2, 3, 4, 5, 6, 7, 'nor_conv_3x3')
+    ],
+    [
+        (1, 3, 'nor_conv_3x3'),
+        (2, 4, 5, 'nor_conv_3x3'),
+        (1, 3, 4, 5, 6, 'nor_conv_3x3'),
+        (2, 4, 6, 7, 'nor_conv_3x3')
+    ],
+    [
+        (1, 2, 3, 4, 'nor_conv_3x3'),
+        (2, 4, 'nor_conv_3x3'),
+        (1, 2, 3, 6, 'nor_conv_3x3'),
+        (1, 2, 4, 6, 7, 'nor_conv_3x3')
+    ]
+])
