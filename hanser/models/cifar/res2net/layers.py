Fix flops calculation for NASNet

hanser/models/cifar/res2net/layers.py
@@ -10,7 +10,7 @@ class StartRes2Conv(Layer):
         super().__init__()
         assert channels % scale == 0
         width = channels // scale
-        self.direct = Identity() if stride == 1 else Pool2d(3, stride, type='avg')
+        self.direct = Pool2d(3, stride, type='avg')
         self.conv = Conv2d(width * (scale - 1), width * (scale - 1),
                            kernel_size=kernel_size, stride=stride,
                            groups=groups * (scale - 1), norm=norm, act=act)
@@ -25,22 +25,20 @@ class StartRes2Conv(Layer):
 
 class MainRes2Conv(Layer):
 
-    def __init__(self, channels, kernel_size, stride=1, scale=4, groups=1, norm='def', act='def'):
+    def __init__(self, channels, kernel_size, scale=4, groups=1, norm='def', act='def'):
         super().__init__()
         assert channels % scale == 0
         width = channels // scale
         self.scale = scale
-        self.direct = Identity() if stride == 1 else Pool2d(3, stride, type='avg')
         self.convs = [
-            Conv2d(width, width, kernel_size=kernel_size, stride=stride, groups=groups, norm=norm, act=act)
+            Conv2d(width, width, kernel_size=kernel_size, groups=groups, norm=norm, act=act)
             for i in range(scale - 1)
         ]
 
     def call(self, x):
         xs = tf.split(x, self.scale, axis=-1)
         outs = [
-            self.direct(xs[0]),
-            self.convs[0](xs[1])
+            xs[0], self.convs[0](xs[1])
         ]
         for i in range(2, self.scale):
             x = outs[-1] + xs[i]
@@ -58,4 +56,4 @@ def Res2Conv(in_channels, out_channels, kernel_size, stride, scale, groups, norm
     if start_block:
         return StartRes2Conv(in_channels, kernel_size, stride, scale, groups, norm, act)
     else:
-        return MainRes2Conv(in_channels, kernel_size, stride, scale, groups, norm, act)
+        return MainRes2Conv(in_channels, kernel_size, scale, groups, norm, act)

hanser/models/nas/operations.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Layer, Concatenate, Lambda
+from hanser.models.modules import Slice
 from hanser.models.layers import Conv2d, Norm, Act, Pool2d
 
 OPS = {
@@ -95,12 +96,14 @@ class FactorizedReduce(Layer):
         super().__init__()
         assert C_out % 2 == 0
         self.act = Act()
+        self.slice = Slice([1, 1, 0], [-1, -1, -1])
         self.conv1 = Conv2d(C_in, C_out // 2, 1, stride=2, bias=False)
         self.conv2 = Conv2d(C_in, C_out // 2, 1, stride=2, bias=False)
         self.norm = Norm(C_out)
+        self.concat = Concatenate()
 
     def call(self, x):
         x = self.act(x)
-        x = tf.concat([self.conv1(x), self.conv2(x[:, 1:, 1:, :])], axis=-1)
+        x = self.concat([self.conv1(x), self.conv2(self.slice(x))])
         x = self.norm(x)
         return x