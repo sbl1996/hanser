277e27a427fa2caa31525b2320dc7b7184761c40
sbl1996 <sbl1996@126.com>
2021-03-15 11:32:50 +0800

    add transformer

---
+++ hanser/models/transformer/modules.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow_addons.layers import GELU
+from tensorflow.keras.layers import Layer, Dense, Dropout, Activation
+
+def safe_softmax(logits, axis):
+    dtype = logits.dtype
+    if dtype in [tf.float16, tf.bfloat16]:
+        logits = tf.cast(logits, tf.float32)
+        weights = tf.nn.softmax(logits, axis=axis)
+        weights = tf.cast(weights, dtype)
+    else:
+        weights = tf.nn.softmax(logits, axis=axis)
+    return weights
+
+
+def scaled_dot_product_attention(q, k, v):
+    matmul_qk = tf.matmul(q, k, transpose_b=True)
+
+    dk = tf.cast(tf.shape(k)[-1], matmul_qk.dtype)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    attention_weights = safe_softmax(scaled_attention_logits, axis=-1)
+
+    output = tf.matmul(attention_weights, v)
+    return output, attention_weights
+
+
+class MultiHeadAttention(Layer):
+    def __init__(self, d_model, num_heads, drop_rate, final_dense=True, **kwargs):
+        super().__init__(**kwargs)
+        self.num_heads = num_heads
+        self.d_model = d_model
+        self.drop_rate = drop_rate
+        self.final_dense = final_dense
+
+        assert d_model % self.num_heads == 0
+
+        self.depth = d_model // self.num_heads
+
+        self.wq = Dense(d_model)
+        self.wk = Dense(d_model)
+        self.wv = Dense(d_model)
+
+        self.dense = Dense(d_model) if final_dense else None
+        self.dropout = Dropout(drop_rate) if drop_rate else None
+
+    def split_heads(self, x):
+        x = tf.reshape(x, (tf.shape(x)[0], -1, self.num_heads, self.depth))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, v, k, q):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)
+        k = self.wk(k)
+        v = self.wv(v)
+
+        q = self.split_heads(q)
+        k = self.split_heads(k)
+        v = self.split_heads(v)
+
+        scaled_attention, attention_weights = scaled_dot_product_attention(q, k, v)
+
+        scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+        output = tf.reshape(scaled_attention,
+                                      (batch_size, -1, self.d_model))
+        if self.dense:
+            output = self.dense(output)
+        if self.dropout:
+            output = self.dropout(output)
+        return output, attention_weights
+
+    def get_config(self):
+        return {
+            **super().get_config(),
+            'num_heads': self.num_heads,
+            'd_model': self.d_model,
+            'drop_rate': self.drop_rate,
+            'final_dense': self.final_dense,
+        }
+
+
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    return pos * angle_rates
+
+def positional_encoding(seq_len, d_model):
+    angle_rads = get_angles(np.arange(seq_len)[:, None], np.arange(d_model)[None, :], d_model)
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    return tf.cast(angle_rads, dtype=tf.float32)
+
+
+def FFN(d_model, dff, drop_rate, activation):
+    layers = [
+        Dense(dff),
+        GELU() if activation == 'gelu' else Activation('relu'),
+        Dense(d_model),
+    ]
+    if drop_rate:
+        layers.insert(2, Dropout(drop_rate))
+        layers.insert(4, Dropout(drop_rate))
+    return Sequential(layers)
---
+++ hanser/models/transformer/pre_ln.py
@@ -0,0 +1,100 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Layer, LayerNormalization, Dropout, Embedding
+from tensorflow.keras.initializers import Constant
+from hanser.models.transformer.modules import MultiHeadAttention, positional_encoding, FFN
+
+
+class TransformerEncoderLayer(Layer):
+    def __init__(self, d_model, num_heads, dff, drop_rate, activation='gelu', **kwargs):
+        super().__init__(**kwargs)
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.dff = dff
+        self.drop_rate = drop_rate
+        self.activation = activation
+
+        self.ln1 = LayerNormalization(epsilon=1e-5)
+        self.mha = MultiHeadAttention(d_model, num_heads, drop_rate)
+
+        self.ffn = FFN(d_model, dff, drop_rate, activation)
+        self.ln2 = LayerNormalization(epsilon=1e-5)
+
+    def call(self, x):
+        identity = x
+        x = self.ln1(x)
+        x, _ = self.mha(x, x, x)
+        x = x + identity
+
+        identity = x
+        x = self.ffn(self.ln2(x))
+        x = x + identity
+        return x
+
+    def get_config(self):
+        return {
+            **super().get_config(),
+            "d_model": self.d_model,
+            "num_heads": self.num_heads,
+            "dff": self.dff,
+            "drop_rate": self.drop_rate,
+            "activation": self.activation,
+        }
+
+
+class TransformerEncoder(Layer):
+    def __init__(self, n_tokens, seq_len, num_layers=4, d_model=256, num_heads=8, dff=1024, drop_rate=0.1, activation='gelu',
+                 **kwargs):
+        super().__init__(**kwargs)
+
+        self.n_tokens = n_tokens
+        self.seq_len = seq_len
+        self.num_layers = num_layers
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.dff = dff
+        self.drop_rate = drop_rate
+        self.activation = activation
+
+        self.embedding = Embedding(n_tokens, d_model)
+        self.pos_embedding = self.add_weight(
+            name="pos_embedding", shape=(seq_len, self.d_model), dtype=self.dtype,
+            initializer=Constant(positional_encoding(seq_len, d_model)), trainable=True,
+        )
+        self.dropout = Dropout(drop_rate) if drop_rate else None
+
+        self.enc_layers = [
+            TransformerEncoderLayer(d_model, num_heads, dff, drop_rate, activation)
+            for i in range(num_layers)
+        ]
+
+        self.ln = LayerNormalization(epsilon=1e-5)
+
+        self.cls_head = Dense(n_tokens)
+
+
+    def call(self, x):
+
+        x = self.embedding(x)
+        x = tf.cast(x, self.pos_embedding.dtype)
+        x += self.pos_embedding[:x.shape[1]]
+        if self.dropout:
+            x = self.dropout(x)
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x)
+        x = self.ln(x)
+
+        x = self.cls_head(x)
+        return x
+
+    def get_config(self):
+        return {
+            **super().get_config(),
+            "n_tokens": self.n_tokens,
+            "seq_len": self.seq_len,
+            'num_layers': self.num_layers,
+            'd_model': self.d_model,
+            'num_heads': self.num_heads,
+            "dff": self.dff,
+            "drop_rate": self.drop_rate,
+            "activation": self.activation,
+        }
