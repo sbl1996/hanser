181bfd8016bfd523a9e9dc6a81db0c21345e46f8
Rederxz <1148002188@qq.com>
2021-08-07 09:49:33 +0800

    Add deep_stem option for resnet_vd.

--- hanser/models/imagenet/resnet_vd.py
+++ hanser/models/imagenet/resnet_vd.py
@@ -1,14 +1,15 @@
 from hanser.models.common.resnet_vd import BasicBlock, Bottleneck
-from hanser.models.imagenet.stem import ResNetvdStem
+from hanser.models.imagenet.stem import ResNetvdStem, ResNetStem
 from hanser.models.imagenet.resnet import _ResNet
 
 
 class ResNet(_ResNet):
 
     def __init__(self, block, layers, num_classes=1000, channels=(64, 64, 128, 256, 512),
-                 maxpool=True, dropout=0.0, zero_init_residual=False):
+                 deep_stem=True, maxpool=True, dropout=0.0, zero_init_residual=False):
         stem_channels, *channels = channels
-        stem = ResNetvdStem(stem_channels, pool=maxpool)
+        Stem = ResNetvdStem if deep_stem else ResNetStem
+        stem = Stem(stem_channels, pool=maxpool)
         super().__init__(stem, block, layers, num_classes, channels,
                          strides=(1 if maxpool else 2, 2, 2, 2),
                          dropout=dropout, zero_init_residual=zero_init_residual)
