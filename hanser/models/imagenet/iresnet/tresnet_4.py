fbf713ced03c54e8529be1f02c43f294fd9764ea
sbl1996 <sbl1996@126.com>
2021-08-09 23:00:08 +0800

    Add tresnet

--- hanser/models/imagenet/iresnet/tresnet_4.py
+++ hanser/models/imagenet/iresnet/tresnet_4.py
@@ -6,27 +6,15 @@ from hanser.models.imagenet.stem import SpaceToDepthStem
 class IResNet(_IResNet):
 
     def __init__(self, block, layers, num_classes=1000, channels=(64, 64, 128, 256, 512),
-                 drop_path=0, anti_alias=True, dropout=0, use_se=False):
+                 drop_path=0, anti_alias=True, dropout=0, use_se=True):
         stem_channels, *channels = channels
         stem = SpaceToDepthStem(stem_channels)
-        se_reduction = (4, 4, 4, 4) if use_se else None
+        se_reduction = 4 if use_se else None
         super().__init__(stem, block, layers, num_classes, channels,
                          strides=(1, 2, 2, 2), dropout=dropout,
                          pool_type='avg', drop_path=drop_path,
                          anti_alias=anti_alias, se_reduction=se_reduction)
 
 
-def resnet18(**kwargs):
-    return IResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
-
-def resnet34(**kwargs):
-    return IResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
-
 def resnet50(**kwargs):
-    return IResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-
-def resnet101(**kwargs):
-    return IResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
-
-def resnet152(**kwargs):
-    return IResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    return IResNet(Bottleneck, [3, 4, 8, 3], **kwargs)
