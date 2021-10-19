a57e0ed035886f640e8d00f27643666aec5ee9f4
sbl1996 <sbl1996@126.com>
2021-10-19 22:08:56 +0800

    Use einsum for aaconv

---
+++ hanser/models/cifar/aaconv2/layers.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+from hanser.models.layers import Conv2d, Pool2d
+
+
+def split_heads_2d(x, n_heads):
+    B = tf.shape(x)[0]
+    H, W, d = x.shape[1:]
+    split = tf.reshape(x, [B, H, W, n_heads, d // n_heads])
+    return split
+
+
+def combine_heads_2d(x):
+    B = tf.shape(x)[0]
+    H, W, n_heads, d = x.shape[1:]
+    return tf.reshape(x, [B, H, W, n_heads * d])
+
+
+def flatten_hw(x):
+    B = tf.shape(x)[0]
+    H, W, n_heads, d = x.shape[1:]
+    return tf.reshape(x, [B, H * W, n_heads, d])
+
+
+def split_hw(x, H, W):
+    # (B, n_heads, H * W, d)
+    B = tf.shape(x)[0]
+    n_heads, d = x.shape[1], x.shape[3]
+    return tf.reshape(x, [B, H, W, n_heads, d])
+
+
+def safe_softmax(logits, axis):
+    dtype = logits.dtype
+    if dtype in [tf.float16, tf.bfloat16]:
+        logits = tf.cast(logits, tf.float32)
+        weights = tf.nn.softmax(logits, axis=axis)
+        weights = tf.cast(weights, dtype)
+    else:
+        weights = tf.nn.softmax(logits, axis=axis)
+    return weights
+
+
+def scaled_dot_product_attention(q, k, v):
+    # q: (N, h*w, n_h, dq / n_h)
+    # k: (N, h*w, n_h, dk / n_h)
+    # v: (N, h*w, n_h, dv / n_h)
+    matmul_qk = tf.einsum('...qhd,...khd->...hqk', q, k)
+
+    dk = tf.cast(tf.shape(k)[-1], matmul_qk.dtype)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    attn_weights = safe_softmax(scaled_attention_logits, axis=-1)
+
+    output = tf.einsum('...hqk,...khd->...qhd', attn_weights, v)
+    return output
+
+
+class MultiHeadAttention(Layer):
+
+    def __init__(self, c_in, n_heads, dk, dv, stride=1):
+        super().__init__()
+        assert dv % n_heads == 0
+        self.n_heads = n_heads
+        self.stride = stride
+        if self.stride == 2:
+            self.downsample = Pool2d(3, 2, type='avg')
+
+        dh_k = max(dk // n_heads, 20)
+        self.Q = Conv2d(c_in, dh_k * n_heads, 1)
+        self.K = Conv2d(c_in, dh_k * n_heads, 1)
+        self.V = Conv2d(c_in, dv, 1)
+
+        self.project = Conv2d(dv, dv, 1)
+
+    def call(self, x):
+        """
+            x: feature map of shape (N, H, W, c_in)
+            :return: (N, H, W, d_v) or (N, H // 2, W // 2, d_v)
+        """
+        if self.stride == 2:
+            x = self.downsample(x)
+
+        H, W = x.shape[1:3]
+
+        q = self.Q(x)
+        k = self.K(x)
+        v = self.V(x)
+
+        q = flatten_hw(split_heads_2d(q, self.n_heads))
+        k = flatten_hw(split_heads_2d(k, self.n_heads))
+        v = flatten_hw(split_heads_2d(v, self.n_heads))
+
+        out = scaled_dot_product_attention(q, k, v)
+        out = combine_heads_2d(split_hw(out, H, W))
+
+        out = self.project(out)
+        return out
+
+
+class AAConv(Layer):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, n_heads, dk, dv):
+        super().__init__()
+        self.conv = Conv2d(in_channels, out_channels - dv, kernel_size, stride=stride)
+        self.att = MultiHeadAttention(in_channels, n_heads, dk, dv, stride=stride)
+
+    def call(self, x):
+        x1 = self.conv(x)
+        x2 = self.att(x)
+        return tf.concat([x1, x2], axis=-1)
---
+++ hanser/models/cifar/aaconv2/preactresnet.py
@@ -0,0 +1,84 @@
+from tensorflow.keras import Sequential, Model
+from tensorflow.keras.layers import Layer
+
+from hanser.models.layers import Act, Conv2d, Norm, GlobalAvgPool, Linear, Identity, Dropout
+from hanser.models.cifar.aaconv2.layers import AAConv
+
+
+class PreActResBlock(Layer):
+    def __init__(self, in_channels, out_channels, stride, dropout=0,
+                 n_heads=8, k=0.2, v=0.1):
+        super().__init__()
+        dk = int(k * out_channels)
+        dv = int(v * out_channels)
+        self.stride = stride
+        self.norm1 = Norm(in_channels)
+        self.act1 = Act()
+        self.conv1 = AAConv(in_channels, out_channels, kernel_size=3, stride=stride,
+                            n_heads=n_heads, dk=dk, dv=dv)
+        self.norm2 = Norm(out_channels)
+        self.act2 = Act()
+        self.dropout = Dropout(dropout) if dropout else Identity()
+        self.conv2 = Conv2d(out_channels, out_channels, kernel_size=3)
+
+        if stride > 1 or in_channels != out_channels:
+            self.shortcut = Conv2d(in_channels, out_channels, kernel_size=1, stride=stride)
+        else:
+            self.shortcut = None
+
+    def call(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.act1(x)
+        if self.shortcut is not None:
+            shortcut = self.shortcut(x)
+        x = self.conv1(x)
+        x = self.norm2(x)
+        x = self.act2(x)
+        x = self.dropout(x)
+        x = self.conv2(x)
+        return x + shortcut
+
+
+class ResNet(Model):
+
+    def __init__(self, depth, widen_factor, dropout=0, num_classes=100,
+                 stages=(16, 16, 32, 64), n_heads=8, k=0.2, v=0.1):
+        super().__init__()
+        num_blocks = (depth - 4) // 6
+        stages = [stages[0], *[c * widen_factor for c in stages[1:]]]
+        strides = [1, 2, 2]
+
+        self.stem = Conv2d(3, stages[0], kernel_size=3)
+
+        for i in range(3):
+            layer = self._make_layer(
+                stages[i], stages[i+1], num_blocks, stride=strides[i],
+                dropout=dropout, n_heads=n_heads, k=k, v=v)
+            setattr(self, "layer" + str(i+1), layer)
+
+        self.norm = Norm(stages[3])
+        self.act = Act()
+        self.avgpool = GlobalAvgPool()
+        self.fc = Linear(stages[3], num_classes)
+
+    def _make_layer(self, in_channels, out_channels, blocks, stride, **kwargs):
+        layers = [PreActResBlock(in_channels, out_channels, stride=stride, **kwargs)]
+        for i in range(1, blocks):
+            layers.append(
+                PreActResBlock(out_channels, out_channels, stride=1, **kwargs))
+        return Sequential(layers)
+
+    def call(self, x):
+        x = self.stem(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.norm(x)
+        x = self.act(x)
+
+        x = self.avgpool(x)
+        x = self.fc(x)
+        return x
