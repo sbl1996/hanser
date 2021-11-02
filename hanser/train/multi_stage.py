2833b495c4051cfcdf977e581d17fa3604cdb2b9
sbl1996 <sbl1996@126.com>
2021-11-02 11:26:55 +0800

    add multi_stage training

---
+++ hanser/train/multi_stage.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from hanser.train.callbacks import Callback
+from hanser.hpo.repeat import repeat
+
+class StageEndException(RuntimeError):
+    pass
+
+class TrainEndException(RuntimeError):
+    pass
+
+class StageControl(Callback):
+
+    priority = -1
+
+    def __init__(self, epochs, n_stages):
+        super().__init__()
+        self.epochs = epochs
+        self.n_stages = n_stages
+
+    def after_eval(self, state):
+        epoch = self.learner.epoch + 1
+        if epoch % (self.epochs // self.n_stages) == 0 and epoch != self.epochs:
+            raise StageEndException
+        if epoch == self.epochs:
+            raise TrainEndException
+
+
+def train_multi_stage(train_fn):
+    train_fn = lambda _i: train_fn(_i)
+    repeat(train_fn, 1000, catch=(tf.errors.UnavailableError, StageEndException))
