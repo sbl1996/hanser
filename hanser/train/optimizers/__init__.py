855d01edf7e1cd7f0c1b180dc6e249a2f4023376
sbl1996 <sbl1996@126.com>
2021-11-27 19:03:17 +0800

    move AdamW

--- hanser/train/optimizers/__init__.py
+++ hanser/train/optimizers/__init__.py
@@ -1,4 +1,4 @@
 from hanser.train.optimizers.sgd import SGD, SGDS, PNM, SGDW
-from hanser.train.optimizers.adamw import AdamW
+from hanser.train.optimizers.adam import AdamW, AdaPNM
 from hanser.train.optimizers.rmsprop import RMSprop
 from hanser.train.optimizers.lamb import LAMB
--- hanser/train/optimizers/adam.py
+++ hanser/train/optimizers/adam.py
@@ -8,36 +8,39 @@ from hanser.train.optimizers.types import FloatTensorLike
 
 
 @tf.keras.utils.register_keras_serializable(package="Hanser")
-class AdaPNM(tf.keras.optimizers.Optimizer):
+class AdamW(tf.keras.optimizers.Optimizer):
 
     @typechecked
     def __init__(
         self,
-        learning_rate: Union[FloatTensorLike, Callable],
-        betas: Tuple[FloatTensorLike, FloatTensorLike] = (0.9, 0.999, 1.0),
-        eps: float = 1e-8,
-        amsgrad: bool = True,
-        weight_decay: FloatTensorLike = 0,
+        learning_rate: Union[FloatTensorLike, Callable] = 0.001,
+        beta_1: FloatTensorLike = 0.9,
+        beta_2: FloatTensorLike = 0.999,
+        epsilon: FloatTensorLike = 1e-8,
+        amsgrad: bool = False,
+        weight_decay: FloatTensorLike = 0.01,
         exclude_from_weight_decay: Optional[List[str]] = None,
-        name: str = "AdaPNM",
+        name='AdamW',
         **kwargs
     ):
         super().__init__(name, **kwargs)
-
-        self._set_hyper("learning_rate", kwargs.get("lr", learning_rate))
-        self._set_hyper("beta1", betas[0])
-        self._set_hyper("beta2", betas[1])
-        self._set_hyper("beta3", betas[2])
-        self._set_hyper("weight_decay", weight_decay)
-
-        self.eps = eps
+        self._set_hyper('learning_rate', kwargs.get('lr', learning_rate))
+        self._set_hyper('decay', self._initial_decay)
+        self._set_hyper('beta_1', beta_1)
+        self._set_hyper('beta_2', beta_2)
+        self._set_hyper('weight_decay', weight_decay)
+        self.epsilon = epsilon
         self.amsgrad = amsgrad
         self.exclude_from_weight_decay = exclude_from_weight_decay
 
     def _create_slots(self, var_list):
         for var in var_list:
-            self.add_slot(var, "pos_m")
-            self.add_slot(var, "neg_m")
+            self.add_slot(var, 'm')
+        for var in var_list:
+            self.add_slot(var, 'v')
+        if self.amsgrad:
+            for var in var_list:
+                self.add_slot(var, 'vhat')
 
     def _do_use_weight_decay(self, param_name):
         if self.exclude_from_weight_decay:
@@ -49,63 +52,81 @@ class AdaPNM(tf.keras.optimizers.Optimizer):
     def _prepare_local(self, var_device, var_dtype, apply_state):
         super()._prepare_local(var_device, var_dtype, apply_state)
 
-        beta1 = tf.identity(self._get_hyper("beta1", var_dtype))
-        beta2 = tf.identity(self._get_hyper("beta2", var_dtype))
-        weight_decay = tf.identity(self._get_hyper("weight_decay", var_dtype))
-        is_even = tf.cast(self.iterations, tf.int32) % 2 == 0
-        noise_norm = tf.sqrt((1 + beta2) ** 2 + beta2 ** 2)
+        local_step = tf.cast(self.iterations + 1, var_dtype)
+        beta_1_t = tf.identity(self._get_hyper('beta_1', var_dtype))
+        beta_2_t = tf.identity(self._get_hyper('beta_2', var_dtype))
+        beta_1_power = tf.pow(beta_1_t, local_step)
+        beta_2_power = tf.pow(beta_2_t, local_step)
+        weight_decay = tf.identity(self._get_hyper('weight_decay', var_dtype))
+        lr = (apply_state[(var_device, var_dtype)]['lr_t'] *
+              (tf.sqrt(1 - beta_2_power) / (1 - beta_1_power)))
         apply_state[(var_device, var_dtype)].update(
             dict(
-                is_even=is_even,
-                beta1=beta1,
-                beta2=beta2,
+                lr=lr,
+                epsilon=tf.convert_to_tensor(self.epsilon, var_dtype),
+                beta_1_t=beta_1_t,
+                beta_1_power=beta_1_power,
+                one_minus_beta_1_t=1 - beta_1_t,
+                beta_2_t=beta_2_t,
+                beta_2_power=beta_2_power,
+                one_minus_beta_2_t=1 - beta_2_t,
                 weight_decay=weight_decay,
-                noise_norm=noise_norm,
-            )
-        )
+            ))
 
     def _resource_apply_dense(self, grad, var, apply_state=None):
         var_device, var_dtype = var.device, var.dtype.base_dtype
-        coefficients = (apply_state or {}).get(
-            (var_device, var_dtype)
-        ) or self._fallback_apply_state(var_device, var_dtype)
+        coefficients = ((apply_state or {}).get((var_device, var_dtype))
+                        or self._fallback_apply_state(var_device, var_dtype))
 
-        pos_m = self.get_slot(var, "pos_m")
-        neg_m = self.get_slot(var, "neg_m")
-
-        beta1, beta2 = coefficients['beta1'], coefficients['beta2']
-
-        def momentum(m):
-            return m * (beta1 ** 2) + grad * (1 - beta1 ** 2)
-
-        is_even = coefficients['is_even']
-        pos_m_t, neg_m_t = tf.cond(
-            is_even,
-            lambda: [momentum(pos_m), neg_m],
-            lambda: [pos_m, momentum(neg_m)])
-
-        pos_m = pos_m.assign(pos_m_t, use_locking=self._use_locking)
-        neg_m = neg_m.assign(neg_m_t, use_locking=self._use_locking)
-
-        pos_m_t, neg_m_t = tf.cond(is_even, lambda: [pos_m_t, neg_m_t], lambda: [neg_m_t, pos_m_t])
-        update = ((1 + beta2) * pos_m_t - beta2 * neg_m_t) / coefficients['noise_norm']
-
-        var_update = var - coefficients["lr_t"] * update
+        m = self.get_slot(var, 'm')
+        v = self.get_slot(var, 'v')
 
         var_name = self._get_variable_name(var.name)
         if self._do_use_weight_decay(var_name):
-            weight_decay = coefficients["lr_t"] * coefficients["weight_decay"]
-            var_update = var_update - weight_decay * var
+            wd = coefficients["weight_decay"] * coefficients["lr_t"]
+            var.assign_sub(wd * var, self._use_locking)
 
-        return var.assign(var_update, use_locking=self._use_locking)
+        if not self.amsgrad:
+            return tf.raw_ops.ResourceApplyAdam(
+                var=var.handle,
+                m=m.handle,
+                v=v.handle,
+                beta1_power=coefficients['beta_1_power'],
+                beta2_power=coefficients['beta_2_power'],
+                lr=coefficients['lr_t'],
+                beta1=coefficients['beta_1_t'],
+                beta2=coefficients['beta_2_t'],
+                epsilon=coefficients['epsilon'],
+                grad=grad,
+                use_locking=self._use_locking)
+        else:
+            vhat = self.get_slot(var, 'vhat')
+            return tf.raw_ops.ResourceApplyAdamWithAmsgrad(
+                var=var.handle,
+                m=m.handle,
+                v=v.handle,
+                vhat=vhat.handle,
+                beta1_power=coefficients['beta_1_power'],
+                beta2_power=coefficients['beta_2_power'],
+                lr=coefficients['lr_t'],
+                beta1=coefficients['beta_1_t'],
+                beta2=coefficients['beta_2_t'],
+                epsilon=coefficients['epsilon'],
+                grad=grad,
+                use_locking=self._use_locking)
 
     def get_config(self):
-        return {
-            **super().get_config(),
-            "learning_rate": self._serialize_hyperparameter("learning_rate"),
-            "betas": (self._serialize_hyperparameter("beta1"), self._serialize_hyperparameter("beta2")),
-            "weight_decay": self._serialize_hyperparameter("weight_decay"),
-        }
+        config = super().get_config()
+        config.update({
+            'learning_rate': self._serialize_hyperparameter('learning_rate'),
+            'decay': self._initial_decay,
+            'beta_1': self._serialize_hyperparameter('beta_1'),
+            'beta_2': self._serialize_hyperparameter('beta_2'),
+            'weight_decay': self._serialize_hyperparameter('weight_decay'),
+            'epsilon': self.epsilon,
+            'amsgrad': self.amsgrad,
+        })
+        return config
 
     def _get_variable_name(self, param_name):
         """Get the variable name from the tensor name."""
