73736cfc97568617cc22149a1503be861da4f7d5
sbl1996 <sbl1996@126.com>
2022-02-25 20:35:20 +0800

    add import

--- hanser/train/metrics/__init__.py
+++ hanser/train/metrics/__init__.py
@@ -1,1 +1,3 @@
+from hanser.train.metrics.common import MeanMetricWrapper
 from hanser.train.metrics.classification import TopKCategoricalAccuracy
+from hanser.train.metrics.detection import MeanAveragePrecision, COCOEval
