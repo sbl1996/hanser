b4ca74a03d1132c8141b1998f4fffbe6afece3cd
sbl1996 <sbl1996@126.com>
2022-03-04 20:51:37 +0800

    add make_voc2007_dataset

--- hanser/datasets/detection/voc.py
+++ hanser/datasets/detection/voc.py
@@ -27,6 +27,27 @@ NUM_EXAMPLES = {
     'val': 4952,
 }
 
+
+def make_voc2007_dataset(
+    batch_size, eval_batch_size, transform, data_dir=None, drop_remainder=None):
+    n_train, n_val = 5011, 4952
+    steps_per_epoch = n_train // batch_size
+    if drop_remainder:
+        val_steps = n_val // eval_batch_size
+    else:
+        val_steps = math.ceil(n_val / eval_batch_size)
+
+    ds_train = tfds.load("voc/2007", split=f"train+validation", data_dir=data_dir,
+                         shuffle_files=True, read_config=tfds.ReadConfig(try_autocache=False, skip_prefetch=True))
+    ds_val = tfds.load("voc/2007", split=f"test", data_dir=data_dir,
+                       shuffle_files=False, read_config=tfds.ReadConfig(try_autocache=False, skip_prefetch=True))
+    ds_train = prepare(ds_train, batch_size, transform(training=True),
+                       training=True, repeat=True)
+    ds_val = prepare(ds_val, eval_batch_size, transform(training=False),
+                     training=False, repeat=False, drop_remainder=drop_remainder)
+    return ds_train, ds_val, steps_per_epoch, val_steps
+
+
 def make_voc_dataset(
     batch_size, eval_batch_size, transform, data_dir=None, drop_remainder=None):
     n_train, n_val = NUM_EXAMPLES['train'], NUM_EXAMPLES['val']
