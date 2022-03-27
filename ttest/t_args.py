dc4debf1fca64f58ec4f1d3256d300257019af28
sbl1996 <sbl1996@126.com>
2022-03-27 16:10:12 +0800

    remove private

--- ttest/t_args.py
+++
@@ -1,5 +0,0 @@
-import argparse
-parser = argparse.ArgumentParser(description='PyTorch ImageNet Training for iResNets')
-parser.add_argument('--nesterov', action='store_true', help='Nesterov momentum')
-args = parser.parse_args()
-print(args)
