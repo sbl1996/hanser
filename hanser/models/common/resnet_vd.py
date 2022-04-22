691f205dfadc1ed129ab6348774aa60b1219d753
sbl1996 <sbl1996@126.com>
2022-04-22 15:34:55 +0800

    fix drop_path in resnet_vd

--- hanser/models/common/resnet_vd.py
+++ hanser/models/common/resnet_vd.py
@@ -1,18 +1,21 @@
 from tensorflow.keras.layers import Layer
 
-from hanser.models.layers import Conv2d, Act, Norm
+from hanser.models.layers import Conv2d, Act, Norm, Identity
+from hanser.models.modules import DropPath
 from hanser.models.common.modules import get_shortcut_vd
+
 
 class BasicBlock(Layer):
     expansion = 1
 
-    def __init__(self, in_channels, channels, stride, zero_init_residual=False, dilation=1):
+    def __init__(self, in_channels, channels, stride, zero_init_residual=False, drop_path=0, dilation=1):
         super().__init__()
         out_channels = channels * self.expansion
         self.conv1 = Conv2d(in_channels, out_channels, kernel_size=3, stride=stride,
                             norm='def', act='def')
         self.conv2 = Conv2d(out_channels, out_channels, kernel_size=3,
                             norm='def', gamma_init='zeros' if zero_init_residual else 'ones')
+        self.drop_path = DropPath(drop_path) if drop_path else Identity()
 
         self.shortcut = get_shortcut_vd(in_channels, out_channels, stride)
 
@@ -22,6 +25,7 @@ class BasicBlock(Layer):
         identity = self.shortcut(x)
         x = self.conv1(x)
         x = self.conv2(x)
+        x = self.drop_path(x)
         x = x + identity
         x = self.act(x)
         return x
@@ -30,7 +34,7 @@ class BasicBlock(Layer):
 class Bottleneck(Layer):
     expansion = 4
 
-    def __init__(self, in_channels, channels, stride, zero_init_residual=False, dilation=1):
+    def __init__(self, in_channels, channels, stride, zero_init_residual=False, drop_path=0, dilation=1):
         super().__init__()
         out_channels = channels * self.expansion
         self.conv1 = Conv2d(in_channels, channels, kernel_size=1,
@@ -39,6 +43,8 @@ class Bottleneck(Layer):
                             dilation=dilation, norm='def', act='def')
         self.conv3 = Conv2d(channels, out_channels, kernel_size=1)
         self.bn3 = Norm(out_channels, gamma_init='zeros' if zero_init_residual else 'ones')
+
+        self.drop_path = DropPath(drop_path) if drop_path else Identity()
 
         self.shortcut = get_shortcut_vd(in_channels, out_channels, stride)
 
@@ -50,6 +56,7 @@ class Bottleneck(Layer):
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.bn3(x)
+        x = self.drop_path(x)
         x = x + identity
         x = self.act(x)
         return x
